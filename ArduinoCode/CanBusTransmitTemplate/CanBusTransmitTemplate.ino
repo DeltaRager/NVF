ba7d300fa23c1dc97275ccea5aa62dadc7a10ea3
DeltaRager <abhinavtj@gmail.com>
2022-10-18 17:54:30 +0800

    Uploading code after successful test

---
+++ ArduinoCode/CanBusTransmitTemplate/CanBusTransmitTemplate.ino
@@ -0,0 +1,29 @@
+#include <SPI.h>
+#include <mcp_can.h>
+
+const int spiCSPin = 10;
+int ledHIGH    = 1;
+int ledLOW     = 0;
+
+MCP_CAN CAN(spiCSPin);
+
+void setup()
+{
+    Serial.begin(115200);
+
+    while (CAN_OK != CAN.begin(CAN_500KBPS,MCP_8MHz))
+    {
+        Serial.println("CAN BUS init Failed");
+        delay(100);
+    }
+    Serial.println("CAN BUS Shield Init OK!");
+}
+
+unsigned char stmp[8] = {7, 6, 5, 4, 3, 2, 1, 0};
+    
+void loop()
+{   
+  Serial.println("In loop");
+  CAN.sendMsgBuf(0x43, 0, 8, stmp);
+  delay(1000);
+} 
